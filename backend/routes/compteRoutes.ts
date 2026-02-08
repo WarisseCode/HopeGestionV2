0ec9c2fa494d2d11898bf095ba12eb68e3e04996
Warisse OTCHADE <warissecodeman@gmail.com>
2026-02-08 19:02:41 +0100

    Nouvelle correction 2*

--- backend/routes/compteRoutes.ts
+++ backend/routes/compteRoutes.ts
@@ -19,8 +19,7 @@ router.get('/proprietaires', async (req: AuthenticatedRequest, res: Response) =>
                    city as ville, country as pays, id_number as "numeroPiece", 
                    photo, management_mode as "modeGestion",
                    mobile_money_number as "mobileMoney", 
-                   id_number as "rccmNumber",
-                   company_name
+                   id_number as "rccmNumber"
             FROM owners
             WHERE is_active = TRUE
         `;
